aabbb2ffa48da6bafac8d3a6b100bc859eace903
Kostia-Hn <gnennyy@gmail.com>
2020-05-17 01:02:17 +0300

    Home Work 8

--- src-K/student/models.py
+++ src-K/student/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import datetime
-
+import random
 # Create your models here.
 from faker import Faker
 
@@ -8,15 +8,27 @@ from faker import Faker
 class Student(models.Model):
     Student_first_name = models.CharField(max_length=40, null=False)
     Student_second_name = models.CharField(max_length=20, null=False)
-    Student_email = models.CharField(max_length=15, null=True)
-    Student_birthday = models.DateField(default=datetime.datetime.now().date())
+    Student_email = models.EmailField(max_length=20, null=True)
+    Student_birthday = models.DateField(default=datetime.date.today)
+    Student_phone_number = models.CharField(max_length=16, null=True)
 
     def __str__(self):
-        return f'{self.id}, {self.Student_first_name}, {self.Student_second_name}, {self.Student_email}'
+        return f'{self.id},' \
+            f' {self.Student_first_name},' \
+            f' {self.Student_second_name},' \
+            f' {self.Student_birthday},' \
+            f' {self.Student_email},' \
+            f' {self.Student_phone_number}'
 
     @classmethod
     def generate_student(cls):
+        a = '+'
+        for _ in range(10):
+            a += str(random.randrange(0, 9, 1))
         faker=Faker()
-        student = cls(Student_first_name=faker.first_name(), Student_second_name=faker.last_name(), Student_email=faker.email())
+        student = cls(Student_first_name=faker.first_name(),
+                      Student_second_name=faker.last_name(),
+                      Student_email=faker.email(),
+                      Student_phone_number=a)
         student.save()
         return student
--- src-K/teacher/models.py
+++ src-K/teacher/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 import datetime
-
+import random
 # Create your models here.
 from faker import Faker
 
@@ -8,16 +8,30 @@ from faker import Faker
 class Teacher(models.Model):
     Teacher_first_name = models.CharField(max_length=40, null=False)
     Teacher_second_name = models.CharField(max_length=20, null=False)
-    Teacher_specializatin = models.CharField(max_length=10, null=True)
+    Teacher_email = models.CharField(max_length=20, null=True)
     Teacher_birthday = models.DateField(default=datetime.datetime.now().date())
+    Teacher_phone_number = models.CharField(max_length=16, null=True)
 
     def __str__(self):
-        return f'{self.id}, {self.Teacher_first_name}, {self.Teacher_second_name}, {self.Teacher_birthday}'
+        return f'{self.id},' \
+            f' {self.Teacher_first_name}, ' \
+            f'{self.Teacher_second_name}, ' \
+            f'{self.Teacher_email}, ' \
+            f'{self.Teacher_phone_number}, ' \
+            f'{self.Teacher_birthday}'
+
 
     @classmethod
     def generate_teacher(cls):
+        a = '+'
+        for _ in range(10):
+            a += str(random.randrange(0,9,1))
         faker=Faker()
-        teacher = cls(Teacher_first_name=faker.first_name(), Teacher_second_name=faker.last_name(), Teacher_specializatin=faker.email())
+        teacher = cls(Teacher_first_name=faker.first_name(),
+                      Teacher_second_name=faker.last_name(),
+                      Teacher_email=faker.email(),
+                      Teacher_phone_number=a)
+
         teacher.save()
         return teacher
 
